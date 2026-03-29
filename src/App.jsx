cd97f2e9a16a81bc46c65c754540e945ae6838a9
apurbochaki-lab <apurbokumarchaki@gmail.com>
2026-03-29 20:29:31 +0600

    Project setup completed by adding font, tailwind+DaisyUI, React Icons & React Toastify

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-4xl text-red-500'>Hello</h2>
+      <button className="btn primary-bg text-white py-5">Sample Button</button>
+    </>
+  )
+}
+
+export default App
